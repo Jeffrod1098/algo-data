8dde38eaafaf5a2395749c69edda0fa0fce2926c
Jeff <mrjeffrey1011@gmail.com>
2022-10-18 20:36:02 -0400

    this is the o(N) way of doing sum of zero array this question

--- algo.js
+++ algo.js
@@ -10,3 +10,23 @@ const sumzero = function(arr){
   }
   
   console.log(sumzero([2,1,0,-1,-2]))
+
+
+
+  // _________________________________________
+// function sumZeroN(arr) {
+//   let left = 0;
+//   let right = arr.length -1;
+//   while(left < right){
+//     let sum = arr[left]+ arr[right];
+//     if(sum === 0){
+//       return [arr[left], arr[right]];
+//     }else if(sum > 0){
+//       right --;
+//     }else{
+//       left++;
+//     }
+//   }
+// }
+
+// console.log(sumZeroN([4,1,0,-1,-2]))
