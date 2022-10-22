66b9c5cc3ff65af2822a3fcefba5dd719730c11b
Jeff <mrjeffrey1011@gmail.com>
2022-10-22 16:15:45 -0400

    Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.

--- algo.js
+++ algo.js
@@ -192,3 +192,27 @@ function solution(str, ending){
   }
 
 
+  // Given an array of integers and a number, write a function called maxSubarraySum, which finds the maximum sum of a subarray with the length of the number passed to the function.
+
+  // Note that a subarray must consist of consecutive elements from the original array. In the first example below, [100, 200, 300] is a subarray of the original array, but [100, 300] is not.
+
+
+function maxSubarrySum(arr, num){
+  if (num > arr.length){
+    return null
+  }
+
+  max = -Infinty 
+  for (let i = 0; i < arr.lenth - num + 1; i++){
+    temp = 0 
+    for (let j = 0; j < num; j++){
+      temp += arr[i + j]
+    }
+    if (temp >= max){
+      max = temp 
+    }
+    return max
+  } 
+}
+
+
